f68df060790f49347f10eb8e2fce2f0a12df114d
Rodrigo Puls <rodrigo.freitas.puls@gmail.com>
2022-08-05 16:50:25 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_s3_bucket_policy" "this" {
   count = var.attach_policy ? 1 : 0
 
   bucket = aws_s3_bucket.this.id
-  policy = data.aws_iam_policy_document.this.json
+  policy = data.aws_iam_policy_document.this[0].json
 }
 
 data "aws_iam_policy_document" "this" {
